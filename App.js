ba2f3a1ba6997d173cb79ecf59293e47cbd091bb
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-31 15:40:35 +0530

    styles date and time

--- App.js
+++ App.js
@@ -5,7 +5,22 @@ import moment from 'moment'
 import KeepAwake from 'react-native-keep-awake'
 
 const styles=StyleSheet.create({
-  
+  container:{
+    flex:1,
+    backgroundColor:'#000',
+    justifyContent:'center',
+    alignItems:'center',
+
+  },
+  timeText:{
+    color: 'white',
+    fontSize:60,
+  },
+  dateText:{
+    color: 'white',
+    fontSize:40,
+  }
+
 })
 export default class App extends Component{
   constructor(props){
@@ -17,9 +32,9 @@ export default class App extends Component{
   }
   render(){
     return(
-      <View>
-        <Text>{this.state.time}</Text>
-        <Text>{this.state.date}</Text>
+      <View style={styles.container}>
+        <Text style={styles.timeText}>{this.state.time}</Text>
+        <Text style={styles.dateText}>{this.state.date}</Text>
       </View>
     )
   }
